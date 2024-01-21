830eafdb2e244d1c04d69e567f0869dc42828a80
DeepikaSivaselvan <deepikas.21ece@kongu.edu>
2024-01-21 21:02:02 +0530

    javascript

---
+++ operators.js
@@ -0,0 +1,31 @@
+//assignment operator
+var a=10
+console.log("Assignment operator output:",a)
+//arithmetic operator
+var aA=10
+var bA=30
+console.log("Arithmetic operator addition output",aA+bA)
+console.log("Arithmetic operator subraction output",aA-bA)
+console.log("Arithmetic operator multiplication output",aA*bA)
+console.log("Arithmetic operator division output",aA/bA)
+console.log("Arithmetic operator modulus output",aA%bA)
+console.log("Arithmetic operator power output",aA**bA)
+console.log("Arithmetic operator Increment output",aA++)
+console.log("Arithmetic operator Decrement output",--bA)
+console.log("Arithmetic operator Increment output",aA++)
+//logical operator --> &&,||,!
+var a1=10
+var b1=20
+ console.log("logical operator AND output",a1&&b1)
+ console.log("logical operator || output",a1||b1)
+ console.log("logical operator ! output",!a1)
+ console.log(10&&20)
+ console.log(10&&30)
+ console.log(0&&40)
+ console.log(10||20)
+ console.log(0||20)
+ console.log(0||20)
+ console.log(100||20&&0)
+ console.log((100||20)&&0)
+ console.log(0&&(100||20))
+ 
---
+++ variable.js
@@ -0,0 +1,12 @@
+a=10
+console.log("general variable declaration",a)
+var b="var"
+console.log("This variable belongs to var type value is:",b)
+var b=14
+console.log("This variable belongs to var type value is:",b)
+let c="let"
+console.log("This variable belongs to changed var type value is:",c)
+const d=100
+console.log("This variable belongs to const type value is",d)
+//d=200 (eror will occur as constant value cant be changed)
+//console.log("This variable belongs to variable type const",d)
